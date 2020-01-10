2df5574b139c1e560f26629d3ee7bde3d8610884
Charles Pierce <cpierce.grad@gmail.com>
2020-01-09 21:46:45 -0800

    Add ini-parse crate to the workspace

---
+++ ini-parse/src/lib.rs
@@ -0,0 +1,2 @@
+// Create intermediate format to parse the full file into, and then serialize from / to that with Serde
+// Can then take advantage of Serde's data model & things like default values into structs
