3159a3acf0c50f11c592caa8fa67279533efdf7d
Charles Pierce <cpierce.grad@gmail.com>
2020-01-21 23:58:02 -0800

    Handle basic escaping and non-existent values

--- ninny/src/parser.rs
+++ ninny/src/parser.rs
@@ -50,6 +50,10 @@ impl<C: Iterator<Item = char>> Parser<C> {
             let map = self.parse_section();
 
             // TODO: Handle tested section titles (with '.' characters in them)
+            // Have `parse_section_title` return a vector of Strings (split on non-escaped '.')
+            // Then do a root.entry(part).or_insert_with(|| Item::Section(HashMap::new())) for each part
+            // For the last we can use `map`, the rest can use `HashMap::new()`
+            // If the final key already exists, then iterate each value and insert, otherwise use it directly
 
             try_insert(&mut root, title, Item::Section(map));
         }
@@ -73,10 +77,19 @@ impl<C: Iterator<Item = char>> Parser<C> {
                     self.consume_comment();
                 }
                 _ => {
-                    let key = self.parse_key();
-                    let value = self.parse_value();
+                    let (key, value) = self.parse_key_value();
 
                     // TODO: Handle a key that ends with [] or a value that is already an array
+                    // Create an Identifier type that knows is_array for []
+                    // When inserting, do the following triage:
+                    //    - If is_array and exists already, look at type
+                    //         - If Array, push to the end
+                    //         - If Value, convert to Array then push to end
+                    //         - If Section, error
+                    //    - Otherwise, Look at existing type
+                    //         - If Array, push to the end
+                    //         - If Value, replace with new value
+                    //         - If Section, error
 
                     try_insert(&mut section, key, Item::Value(value));
                 }
@@ -94,6 +107,9 @@ impl<C: Iterator<Item = char>> Parser<C> {
         // Consume the initial '[' character
         self.step();
         while let Some(c) = self.ch {
+            // TODO: Parse multiple titles separated by '.'
+            // Also need to run out the line and make sure there aren't any
+            // extra characters on the same line as the section (other than whitespace / comments)
             match c {
                 '\r' | '\n' => {
                     panic!("Unclosed section header. Expected ']', found end of line.");
@@ -112,44 +128,67 @@ impl<C: Iterator<Item = char>> Parser<C> {
         panic!("Unclosed section header. Expected ']', found end of file.");
     }
 
-    fn parse_key(&mut self) -> String {
-        let mut key = String::new();
-
-        while let Some(c) = self.ch {
-            match c {
-                '\r' | '\n' | ';' | '#' => {
-                    panic!("Key found with no value. Expected '=', found end of line.");
-                }
-                '=' => {
-                    self.step();
-                    return key.trim().to_string();
-                }
-                _ => {
-                    self.step();
-                    key.push(c);
-                }
-            }
-        }
-
-        panic!("Key found with no value. Expected '=', found end of line.");
-    }
-
-    fn parse_value(&mut self) -> String {
+    fn parse_line_until(&mut self, end: &[char]) -> (bool, String) {
         let mut value = String::new();
+        let mut escape = false;
 
-        while let Some(c) = self.ch {
-            match c {
-                '\r' | '\n' | ';' | '#' => {
-                    break;
+        let found = loop {
+            match self.ch {
+                None | Some('\r') | Some('\n') => {
+                    break false;
                 }
-                _ => {
-                    self.step();
+                Some(c) if c == ';' || c == '#' => {
+                    if escape {
+                        value.push(c);
+                        escape = false;
+                    } else {
+                        break false;
+                    }
+                }
+                Some(e) if end.contains(&e) => {
+                    break true;
+                }
+                Some('\\') => {
+                    if escape {
+                        value.push('\\');
+                        escape = false;
+                    } else {
+                        escape = true;
+                    }
+                }
+                Some(c) => {
+                    if escape {
+                        value.push('\\');
+                        escape = false;
+                    }
+
                     value.push(c);
                 }
             }
+            self.step();
+        };
+
+        if escape {
+            value.push('\\');
         }
 
-        value.trim().to_string()
+        // TODO: Handle quoting by checking if the first and last character are the same quote
+        // If they are, strip those off before returning
+
+        (found, value.trim().to_string())
+    }
+
+    fn parse_key_value(&mut self) -> (String, String) {
+        let (has_value, key) = self.parse_line_until(&['=']);
+
+        if has_value {
+            self.step(); // Consume the '=' character
+            let (_, value) = self.parse_line_until(&[]);
+            (key, value)
+        } else {
+            // To match the ini npm module's behavior, if a key doesn't exist, use the value "true"
+            (key, String::from("true"))
+        }
     }
 }
 
